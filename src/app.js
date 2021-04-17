d28ad1668cc58d263e61ff8b5f5a1b5ceae0576f
architectsanchez13@gmail.com <you@example.com>
2021-04-17 00:00:33 -0500

    tested databases

--- src/app.js
+++ src/app.js
@@ -4,31 +4,45 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(cors())
 app.use(helmet())
 
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles)
+    })
+    .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      res.json(article)
+    })
+    .catch(next)
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-      response = { error: { message: 'server error' } }
-    } else {
-      console.error(error)
-      response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
-
-app.use(cors())
 
 module.exports = app
