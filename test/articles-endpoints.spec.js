386c632b90e49aaa36273d8303b5473012fce49a
architectsanchez13@gmail.com <you@example.com>
2021-04-18 23:31:50 -0500

    Introduced POST request

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,5 +1,6 @@
 const knex = require('knex')
 const app = require('../src/app')
+const { makeArticlesArray } = require('./articles.fixtures')
 
 describe('Articles Endpoints', function() {
   let db
@@ -18,56 +19,88 @@ describe('Articles Endpoints', function() {
 
   afterEach('cleanup',() => db('blogful_articles').truncate())
 
-  context('Given there are articles in the database', () => {
-    const testArticles = [
-      {
-        id: 1,
-        date_published: '2029-01-22T16:28:32.615Z',
-        title: 'First test post!',
-        style: 'How-to',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
-      },
-      {
-        id: 2,
-        date_published: '2100-05-22T16:28:32.615Z',
-        title: 'Second test post!',
-        style: 'News',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
-      },
-      {
-        id: 3,
-        date_published: '1919-12-22T16:28:32.615Z',
-        title: 'Third test post!',
+  describe(`GET /articles`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200, [])
+      })
+    })
+
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+
+      it('responds with 200 and all of the articles', () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200, testArticles)
+      })
+    })
+  })
+
+  describe(`GET /articles/:article_id`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const articleId = 123456
+        return supertest(app)
+          .get(`/articles/${articleId}`)
+          .expect(404, { error: { message: `Article doesn't exist` } })
+      })
+    })
+
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+
+      it('responds with 200 and the specified article', () => {
+        const articleId = 2
+        const expectedArticle = testArticles[articleId - 1]
+        return supertest(app)
+          .get(`/articles/${articleId}`)
+          .expect(200, expectedArticle)
+      })
+    })
+  })
+
+  describe(`POST /articles`, () => {
+    it(`creates an article, responding with 201 and the new article`, function() {
+      this.retries(3)
+      const newArticle = {
+        title: 'Test new article',
         style: 'Listicle',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
-      },
-      {
-        id: 4,
-        date_published: '1919-12-22T16:28:32.615Z',
-        title: 'Fourth test post!',
-        style: 'Story',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?'
-      },
-    ];
-
-    beforeEach('insert articles', () => {
-      return db
-        .into('blogful_articles')
-        .insert(testArticles)
-    })
-
-    it('GET /articles responds with 200 and all of the articles', () => {
+        content: 'Test new article content...'
+      }
       return supertest(app)
-        .get('/articles')
-        .expect(200, testArticles)
-    })
-
-    it('GET /articles/:article_id responds with 200 and the specified article', () => {
-      const articleId = 2
-      const expectedArticle = testArticles[articleId - 1]
-      return supertest(app)
-        .get(`/articles/${articleId}`)
-        .expect(200, expectedArticle)
+        .post('/articles')
+        .send(newArticle)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.title).to.eql(newArticle.title)
+          expect(res.body.style).to.eql(newArticle.style)
+          expect(res.body.content).to.eql(newArticle.content)
+          expect(res.body).to.have.property('id')
+          expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+          const expected = new Date().toLocaleString()
+          const actual = new Date(res.body.date_published).toLocaleString()
+          expect(actual).to.eql(expected)
+        })
+        .then(res =>
+          supertest(app)
+            .get(`/articles/${res.body.id}`)
+            .expect(res.body)
+        )
     })
   })
 })
